74c0dacf367f81cdf3f535b191702d05959b3b0d
Pasham Karthik Reddy <pashamkarthikreddy2005@gmail.com>
2025-03-27 10:39:24 +0530

    Update CorsConfig.java

--- src/main/java/com/example/ReferEase/Config/CorsConfig.java
+++ src/main/java/com/example/ReferEase/Config/CorsConfig.java
@@ -1,25 +1,21 @@
 package com.example.ReferEase.Config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
-    @Value("${frontend-url}")
-    private String fontend;
     @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("https://refer-ease-frontend-as5q.vercel.app/")
+                        .allowedOrigins("https://refer-ease-frontend-as5q.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
